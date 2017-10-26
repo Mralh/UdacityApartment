2e0ddcc35813ade415a982140e8c52fae44d3098
Mralh <mralhamm@gmail.com>
2017-10-26 18:29:36 -0400

    init

---
+++ Assets/Plugins/RealtimeCSG/Runtime/Scripts/Components/Shape/TexGen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using System.Runtime.InteropServices;
+using RealtimeCSG;
+
+namespace InternalRealtimeCSG
+{
+    [Serializable]
+	[StructLayout(LayoutKind.Sequential, Pack = 4)]
+	public struct TexGen
+    {
+		public Color		Color;
+        public Vector2		Translation;
+        public Vector2		Scale;
+        public float		RotationAngle;
+        public Int32		RenderMaterialIndex;		// <- index in material list of owning CSG-model
+        public Int32		PhysicsMaterialIndex;		// <- index in material list of owning CSG-model
+        public UInt32		SmoothingGroup;
+		
+		public TexGen(Int32 renderMaterialIndex = -1, Int32 physicsMaterialIndex = -1)
+		{
+			Color					= Color.white;
+			Translation				= MathConstants.zeroVector3;
+			Scale					= MathConstants.oneVector3;
+			RotationAngle			= 0;
+			RenderMaterialIndex		= renderMaterialIndex;
+			PhysicsMaterialIndex	= physicsMaterialIndex;
+			SmoothingGroup			= 0;
+		}
+    }
+}
---
+++ Assets/Plugins/RealtimeCSG/Runtime/Scripts/Components/Shape/TexGenFlags.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RealtimeCSG
+{
+	// mirrored on C++ side
+	[Flags]
+	public enum TexGenFlags : int //32 bits
+	{
+		None				= 0,
+		WorldSpaceTexture	= 1,
+		Discarded			= 2,		// do not render
+		ShadowOnly			= 4,
+		NoCollision			= 8,		// do not add surface to collider
+		HelperSurface		= 1<<30
+	};
+}
